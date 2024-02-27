c9dfd0ee9761334d9400bc76bc067b539c6824ef
CbnMarian <m.marian03@yahoo.com>
2024-02-27 16:54:12 +0200

    page users

---
+++ admin/users.php
@@ -0,0 +1,69 @@
+<?php include 'includes/admin_header.php' ?>
+
+
+
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- navigation -->
+        <?php include 'includes/admin_navigation.php' ?>
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Author</small>
+                        </h1>
+
+
+                        <?php
+
+                        if (isset($_GET['source'])) {
+                            $source = $_GET['source'];
+                        } else {
+                            $source = '';
+                        }
+
+                        switch ($source) {
+
+                            case 'add_post';
+                                include 'includes/add_user.php';
+                                break;
+
+                            case 'edit_post';
+                                include './edit_user.php';
+                                break;
+
+                            case '33214';
+                                echo "NICE 33214";
+                                break;
+
+
+                            default:
+                                include 'includes/view_all_users.php';
+                                break;
+                        }
+
+
+
+
+
+                        ?>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+        <?php include 'includes/admin_footer.php' ?>
