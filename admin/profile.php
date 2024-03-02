0153185d884bded3d10e53e01d2bc434e5f32d79
CbnMarian <m.marian03@yahoo.com>
2024-03-02 13:30:35 +0200

    added new html for home page admin

--- admin/profile.php
+++ admin/profile.php
@@ -17,6 +17,37 @@ if (isset($_SESSION['username'])) {
     }
 }
 ?>
+<?php
+if (isset($_POST['edit_user'])) {
+
+    $user_firstname  = $_POST['user_firstname'];
+    $user_lastname   = $_POST['user_lastname'];
+    $user_role       = $_POST['user_role'];
+
+    $username        = $_POST['username'];
+    $user_email      = $_POST['user_email'];
+    $user_password   = $_POST['user_password'];
+
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE users SET ";
+    $query .= "user_firstname = '{$user_firstname}', ";
+    $query .= "user_lastname = '{$user_lastname}', ";
+    $query .= "user_role = '{$user_role}', ";
+    $query .= "username = '{$username}', ";
+    $query .= "user_email = '{$user_email}', ";
+    $query .= "user_password = '{$user_password}' ";
+    $query .= "WHERE username = '{$username}' ";
+
+    $edit_user_query = mysqli_query($connection, $query);
+
+    confirm($edit_user_query);
+}
+?>
+
+
+
+
 
 
 <body>
