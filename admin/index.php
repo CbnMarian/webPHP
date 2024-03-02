7cf7c4e8c467b9e710c57be978881168f44d3388
CbnMarian <m.marian03@yahoo.com>
2024-03-02 14:09:20 +0200

    ref3

--- admin/index.php
+++ admin/index.php
@@ -166,17 +166,22 @@
 
                         function drawChart() {
                             var data = google.visualization.arrayToDataTable([
-                                ['Year', 'Sales', 'Expenses', 'Profit'],
-                                ['2014', 1000, 400, 200],
-                                ['2015', 1170, 460, 250],
-                                ['2016', 660, 1120, 300],
-                                ['2017', 1030, 540, 350]
-                            ]);
+                                ['Data', 'Count'],
 
+                                <?php
+                                $elements_text = ['Active Posts', 'Comments', 'Users', 'Categories'];
+                                $elements_count = [$post_count, $comment_count, $user_count, $category_count];
+
+                                for ($i = 0; $i < 4; $i++) {
+                                    echo "['{$elements_text[$i]}'" . "," . "{$elements_count[$i]}],";
+                                }
+
+                                ?>
+                            ]);
                             var options = {
                                 chart: {
-                                    title: 'Company Performance',
-                                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                                    title: '',
+                                    subtitle: '',
                                 }
                             };
 
@@ -186,7 +191,7 @@
                         }
                     </script>
 
-                    <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+                    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
 
                 </div>
 
