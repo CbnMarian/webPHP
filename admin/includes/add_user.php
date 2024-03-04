b2e49a6aea6679234f25e01ded2765f7968b37ff
CbnMarian <m.marian03@yahoo.com>
2024-03-04 17:00:54 +0200

    refact2

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -20,6 +20,7 @@ if (isset($_POST['create_user'])) {
     $create_post_query = mysqli_query($connection, $query);
 
     confirm($create_post_query);
+    echo "User Created: " . " " . "<a href='users.php'>View users</a> ";
 }
 
 
