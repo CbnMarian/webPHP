cafdc9af40f61e0edaa4dd16fd184adb4e6ffc77
CbnMarian <m.marian03@yahoo.com>
2024-03-05 16:01:06 +0200

    4 refactoring

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,83 +1,101 @@
-<table class="table table-bordered table-hover">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Status</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
-            <th>Date</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-
-        <?php
-
-        global $connection;
-        $query = "SELECT * FROM posts";
-        $select_posts = mysqli_query($connection, $query);
-
-        while ($row = mysqli_fetch_assoc($select_posts)) {
-            $post_id = $row['post_id'];
-            $post_author = $row['post_author'];
-            $post_title = $row['post_title'];
-            $post_category_id = $row['post_category_id'];
-            $post_status = $row['post_status'];
-            $post_image = $row['post_image'];
-            $post_tags = $row['post_tags'];
-            $post_comment_count = $row['post_comment_count'];
-            $post_date = $row['post_date'];
-
-            echo "<tr>";
-            echo "<td>$post_id</td>";
-            echo "<td>$post_author</td>";
-            echo "<td>$post_title</td>";
+<form action="" method="post">
 
 
 
-            // Select category data based on cat_id
-            $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
-            $select_categories_id = mysqli_query($connection, $query);
+    <table class="table table-bordered table-hover">
 
-            // Check if data is fetched
-            while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
+        <div id="bulkOptionsContainer" class="col-xs-4">
+            <select name="form-control" id="">
+                <option value="">Select Options</option>
+                <option value="">Publish</option>
+                <option value="">Draft</option>
+                <option value="">Deletet</option>
+            </select>
+        </div>
+        <dic class="col-xs-4">
+            <input type="submit" name="submit" class="btn btn-success" value="Apply">
+            <a class="btn btn-primary" href="add_post.php">Add New</a>
+        </dic>
+
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Author</th>
+                <th>Title</th>
+                <th>Category</th>
+                <th>Status</th>
+                <th>Image</th>
+                <th>Tags</th>
+                <th>Comments</th>
+                <th>Date</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php
+
+            global $connection;
+            $query = "SELECT * FROM posts";
+            $select_posts = mysqli_query($connection, $query);
+
+            while ($row = mysqli_fetch_assoc($select_posts)) {
+                $post_id = $row['post_id'];
+                $post_author = $row['post_author'];
+                $post_title = $row['post_title'];
+                $post_category_id = $row['post_category_id'];
+                $post_status = $row['post_status'];
+                $post_image = $row['post_image'];
+                $post_tags = $row['post_tags'];
+                $post_comment_count = $row['post_comment_count'];
+                $post_date = $row['post_date'];
+
+                echo "<tr>";
+                echo "<td>$post_id</td>";
+                echo "<td>$post_author</td>";
+                echo "<td>$post_title</td>";
 
 
-                echo "<td>{$cat_title}</td>";
+
+                // Select category data based on cat_id
+                $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+                $select_categories_id = mysqli_query($connection, $query);
+
+                // Check if data is fetched
+                while ($row = mysqli_fetch_assoc($select_categories_id)) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+
+
+                    echo "<td>{$cat_title}</td>";
+                }
+
+
+
+
+
+                echo "<td>$post_status</td>";
+                echo "<td><img width='100' src='../images/$post_image' alt='img'></td>";
+                echo "<td>$post_tags</td>";
+                echo "<td> $post_comment_count</td>";
+                echo "<td> $post_date</td>";
+                echo "<td><a href='post.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+                echo "<td><a href='post.php?delete={$post_id}'>Delete</a></td>";
+
+
+                echo "</tr>";
             }
 
+            ?>
 
 
 
 
-            echo "<td>$post_status</td>";
-            echo "<td><img width='100' src='../images/$post_image' alt='img'></td>";
-            echo "<td>$post_tags</td>";
-            echo "<td> $post_comment_count</td>";
-            echo "<td> $post_date</td>";
-            echo "<td><a href='post.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-            echo "<td><a href='post.php?delete={$post_id}'>Delete</a></td>";
+        </tbody>
+    </table>
 
-
-            echo "</tr>";
-        }
-
-        ?>
-
-
-
-
-    </tbody>
-</table>
-
-
+</form>
 <?php
 
 if (isset($_GET['delete'])) {
