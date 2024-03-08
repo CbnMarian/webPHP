f32504cafa8dc2ca57bcc94b286deafb79ee6ca8
CbnMarian <m.marian03@yahoo.com>
2024-03-08 13:47:34 +0200

    adding new funct for add post

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -54,6 +54,7 @@ if (isset($_POST['create_post'])) {
     </div>
 
     <div class="form-group">
+        <label for="category">Category</label>
 
         <select name="post_category" id="post_category">
             <?php
@@ -75,10 +76,41 @@ if (isset($_POST['create_post'])) {
 
     </div>
 
+
+
+
+
     <div class="form-group">
+        <label for="users">Users</label>
+
+        <select name="post_users" id="post_users">
+            <?php
+            $users_query = "SELECT * FROM users";
+            $select_users = mysqli_query($connection, $users_query);
+
+            confirm($select_users);
+
+
+            while ($row = mysqli_fetch_assoc($select_users)) {
+                $user_id = $row['user_id'];
+                $username = $row['username'];
+
+                echo "<option value='$user_id'>{$username}</option>";
+            }
+
+            ?>
+        </select>
+
+    </div>
+
+
+
+
+
+    <!-- <div class="form-group">
         <label for="title">Post Author</label>
         <input type="text" class="form-control" name="author">
-    </div>
+    </div> -->
 
     <div class="form-group">
 
