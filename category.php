193c884a9d8ea74055e0044bb26c20b12dc8b5f5
CbnMarian <m.marian03@yahoo.com>
2024-03-13 12:39:10 +0200

    refact4

--- category.php
+++ category.php
@@ -22,8 +22,15 @@
                 $post_category_id = $_GET['category'];
 
 
+                if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin') {
+                    $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id";
+                } else {
+                    $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published' ";
+                }
 
-                $query = "SELECT * FROM posts WHERE post_category_id = $post_category_id AND post_status = 'published' ";
+
+
+
 
                 $select_all_posts_query = mysqli_query($connection, $query);
 
